0223b7c423bbd17c5a038f3cb3691a729adff3e9
vipulghadi <vipulvijayghadi@gmail.com>
2024-01-27 01:24:15 +0530

    created

---
+++ 1.js
@@ -0,0 +1,18 @@
+function removeAtInd(str, ind) {
+    if (str.length > ind) { return str.splice(ind, 1) }
+    return str
+
+}
+console.log(removeAtInd("vipul", 2));
+
+function f1() {
+    return 1
+}
+
+function f2() {
+    return 2
+}
+
+function f3() {
+    return 3
+}
